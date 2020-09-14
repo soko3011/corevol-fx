881891309dfa5e4135318c6749136032b51478aa
Jamie Sokolsky <jamiesokolsky@jamies-mbp.home>
2020-09-14 14:25:15 +0100

    updates

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,15 @@ module.exports = {
   outputDir: "docs",
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "corevol-fx",
+        icon: "/Users/jamiesokolsky/vuetify-ui/src/assets/icon.icns",
+        mac: {
+          category: "finance",
+        },
+      },
+    },
+  },
 };

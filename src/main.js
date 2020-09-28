2ec4bf2b8a26c578b217a9ab30c0670f989a44c4
Wimpole <wimpole@Wimpoles-Mac-mini.local>
2020-09-28 14:07:09 +0100

    adding tabulator

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import VueTabulator from "vue-tabulator";
 
-Vue.config.productionTip = false
+Vue.use(VueTabulator);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
+  VueTabulator,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,8 +1,72 @@
-<template></template>
+<template>
+  <div>
+    <div ref="table"></div>
+    <VueTabulator v-model="tableData" :options="options" />
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      tableData: [
+        { id: 1, name: "Oli Bob", age: "12", col: "red", dob: "" },
+        { id: 2, name: "Mary May", age: "1", col: "blue", dob: "14/05/1982" },
+        {
+          id: 3,
+          name: "Christine Lobowski",
+          age: "42",
+          col: "green",
+          dob: "22/05/1982"
+        },
+        {
+          id: 4,
+          name: "Brendon Philips",
+          age: "125",
+          col: "orange",
+          dob: "01/08/1980"
+        },
+        {
+          id: 5,
+          name: "Margret Marmajuke",
+          age: "16",
+          col: "yellow",
+          dob: "31/01/1999"
+        }
+      ],
+      options: {
+        columns: [
+          { title: "Name", field: "name", width: 150 },
+          {
+            title: "Age",
+            field: "age",
+            hozAlign: "left",
+            formatter: "progress"
+          },
+          { title: "Favourite Color", field: "col" },
+          { title: "Date Of Birth", field: "dob", hozAlign: "center" },
+          {
+            title: "Rating",
+            field: "rating",
+            hozAlign: "center",
+            formatter: "star"
+          },
+          {
+            title: "Passed?",
+            field: "passed",
+            hozAlign: "center",
+            formatter: "tickCross"
+          }
+        ] //define table columns
+      }
+
+      //data for table to display
+    };
+  },
+  watch: {}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4";
 </style>

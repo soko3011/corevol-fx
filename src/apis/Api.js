7fe7a34898e59d9ff7b0453892d8fd353e4ce5e6
Jamie Sokolsky <soko34@gmail.com>
2021-01-16 10:56:00 +0000

    ups

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 import store from "@/store/index.js";
 
-export const base = "https://corevolapi-swfmuuo65q-ez.a.run.app/";
-//export const base = "http://localhost:5002/";
+//export const base = "https://corevolapi-swfmuuo65q-ez.a.run.app/";
+export const base = "http://localhost:5002/";
 
 const baseURL = `${base}api/`;
 

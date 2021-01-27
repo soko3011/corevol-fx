3261206a687a5e678b3910e37a62d2ced7d8799e
jamie sokolsky <soko34@gmail.com>
2021-01-27 13:28:17 +0000

    prod

--- src/views/MainMobile.vue
+++ src/views/MainMobile.vue
@@ -1,29 +1,37 @@
 <template>
-  <v-app class="grey darken-4">
-    <div class="adjustCenter">
-      <v-card color="grey darken-4" flat>
-        <v-img height="125" src="@/logo/corevollogo.png" />
-      </v-card>
-      <div
-        align="center"
-        class="ml-10 mt-10 subtitle-2 font-weight-light text-center text-uppercase red--text text--lighten-0"
-      >
-        mobile coming soon
-
-        <br />
-        <br />
-        <strong
-          class="subtitle-2 font-weight-thin text-center text-uppercase blue--text text--lighten-2"
+  <transition name="fade">
+    <v-app v-if="loading" class="grey darken-4">
+      <div class="adjustCenter">
+        <v-card color="grey darken-4" flat>
+          <v-img height="125" src="@/logo/corevollogo.png" />
+        </v-card>
+        <div
+          align="center"
+          class="ml-10 mt-10 subtitle-2 font-weight-light text-center text-uppercase red--text text--lighten-0"
         >
-          Please visit app from a mac or pc
-        </strong>
+          mobile coming soon
+
+          <br />
+          <br />
+          <strong
+            class="subtitle-2 font-weight-thin text-center text-uppercase blue--text text--lighten-2"
+          >
+            Please visit app from a mac or pc
+          </strong>
+        </div>
       </div>
-    </div>
-  </v-app>
+    </v-app>
+  </transition>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    loading() {
+      return this.$store.state.appLoaded;
+    },
+  },
+};
 </script>
 
 <style>
